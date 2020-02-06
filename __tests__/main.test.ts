2371dd8c92a16a65c3be9be299c8c14894adafc7
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2020-02-06 17:14:54 +0900

    test: add more assert in main.ts test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -24,66 +24,79 @@ describe('main.ts', () => {
     mocked(core.getInput).mockImplementation((name) => name)
   })
 
-  test('ends with warning if isSupportedEvent() is false', async () => {
-    // Arrange
-    mocked(isSupportedEvent).mockReturnValue(false)
-    // Act
-    await run()
-    // Assert
-    expect(core.warning).toHaveBeenCalledTimes(1)
-    expect(core.getInput).not.toHaveBeenCalled()
-    expect(core.setFailed).not.toHaveBeenCalled()
-    expect(sendCommentAsync).not.toHaveBeenCalled()
+  afterEach(() => {
+    // restore process.env
+    process.env = oldEnv
   })
-  test('fails if GITHUB_REPOSITORY is not set', async () => {
-    // Arrange
-    mocked(isSupportedEvent).mockReturnValue(true)
-    // Act
-    await run()
-    // Assert
-    expect(core.setFailed).toHaveBeenCalledTimes(1)
-    expect(core.getInput).not.toHaveBeenCalled()
-    expect(sendCommentAsync).not.toHaveBeenCalled()
-  })
-  test.each([null, '', 'not lgtm'])(
-    'never calls sendCommentAsync if comment is not LGTM',
-    async (comment) => {
+
+  describe('run()', () => {
+    test('ends with warning if isSupportedEvent() is false', async () => {
       // Arrange
-      process.env.GITHUB_EVENT_NAME = 'event_name'
-      process.env.GITHUB_REPOSITORY = 'owner/repo'
-      mocked(isSupportedEvent).mockReturnValue(true)
-      mocked(getEventWebhook).mockReturnValue({
-        comment,
-        issueNumber: 1
-      })
+      mocked(isSupportedEvent).mockReturnValue(false)
       // Act
       await run()
       // Assert
-      expect(core.getInput).toHaveBeenCalledTimes(2)
-      expect(core.info).toHaveBeenCalledTimes(1)
-      expect(core.info).toHaveBeenCalledWith('Comment is not LGTM.')
+      expect(core.warning).toHaveBeenCalledTimes(1)
+      expect(core.getInput).not.toHaveBeenCalled()
       expect(core.setFailed).not.toHaveBeenCalled()
       expect(sendCommentAsync).not.toHaveBeenCalled()
-    }
-  )
-  test.each(['LGTM', 'lgtm'])(
-    'calls sendCommentAsync if comment is LGTM',
-    async (comment) => {
+    })
+    test('fails if GITHUB_REPOSITORY is not set', async () => {
       // Arrange
-      process.env.GITHUB_EVENT_NAME = 'event_name'
-      process.env.GITHUB_REPOSITORY = 'owner/repo'
       mocked(isSupportedEvent).mockReturnValue(true)
-      mocked(getEventWebhook).mockReturnValue({
-        comment,
-        issueNumber: 1
-      })
       // Act
       await run()
       // Assert
-      expect(core.getInput).toHaveBeenCalledTimes(2)
-      expect(core.info).not.toHaveBeenCalled()
-      expect(core.setFailed).not.toHaveBeenCalled()
-      expect(sendCommentAsync).toHaveBeenCalledTimes(1)
-    }
-  )
+      expect(core.setFailed).toHaveBeenCalledTimes(1)
+      expect(core.getInput).not.toHaveBeenCalled()
+      expect(sendCommentAsync).not.toHaveBeenCalled()
+    })
+    test.each([null, '', 'not lgtm'])(
+      'never calls sendCommentAsync if comment is not LGTM',
+      async (comment) => {
+        // Arrange
+        process.env.GITHUB_EVENT_NAME = 'event_name'
+        process.env.GITHUB_REPOSITORY = 'owner/repo'
+        mocked(isSupportedEvent).mockReturnValue(true)
+        mocked(getEventWebhook).mockReturnValue({
+          comment,
+          issueNumber: 1
+        })
+        // Act
+        await run()
+        // Assert
+        expect(core.getInput).toHaveBeenCalledTimes(2)
+        expect(core.info).toHaveBeenCalledTimes(1)
+        expect(core.info).toHaveBeenCalledWith('Comment is not LGTM.')
+        expect(core.setFailed).not.toHaveBeenCalled()
+        expect(sendCommentAsync).not.toHaveBeenCalled()
+      }
+    )
+    test.each(['LGTM', 'lgtm'])(
+      'calls sendCommentAsync if comment is LGTM',
+      async (comment) => {
+        // Arrange
+        process.env.GITHUB_REPOSITORY = 'owner/repo'
+        mocked(isSupportedEvent).mockReturnValue(true)
+        mocked(getEventWebhook).mockReturnValue({
+          comment,
+          issueNumber: 1
+        })
+        // Act
+        await run()
+        // Assert
+        expect(core.getInput).toHaveBeenCalledTimes(2)
+        expect(core.info).not.toHaveBeenCalled()
+        expect(core.setFailed).not.toHaveBeenCalled()
+        expect(sendCommentAsync).toHaveBeenCalledTimes(1)
+        expect(sendCommentAsync).toHaveBeenCalledWith(
+          'token',
+          'owner',
+          'repo',
+          1,
+          '![LGTM](image-url)'
+        )
+      }
+    )
+  })
 })
