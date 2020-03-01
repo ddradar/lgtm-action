3e4e4ec85d2ee4f00f7e0581baecece1a535e48e
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-01 17:58:44 +0900

    feat: add default param

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -9,7 +9,7 @@ type InputParameter = {
 export const getInputParams = (): InputParameter => ({
   token: getInput('token', { required: true }),
   imageUrl: getInput('image-url', { required: true }),
-  searchPattern: getInput('search-pattern')
+  searchPattern: (getInput('search-pattern') || '^(lgtm|LGTM)$')
     .split('\n')
     .filter((x) => x !== '')
     .map((x) => new RegExp(x, 'm'))
