a10785b554af8a4d918f5db1f2eb19c99acacc53
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-01 20:52:37 +0900

    refactor: split getGithubStatus()

--- src/input-helper.ts
+++ src/input-helper.ts
@@ -14,3 +14,20 @@ export const getInputParams = (): InputParameter => ({
     .filter((x) => x !== '')
     .map((x) => new RegExp(x, 'm'))
 })
+
+type GithubStatus = {
+  eventName: string
+  repository: string
+}
+
+export const getGithubStatus = (): GithubStatus => {
+  const eventName = process.env.GITHUB_EVENT_NAME
+  if (!eventName)
+    throw new Error('GITHUB_EVENT_NAME is not set in an environment variable.')
+  const repository = process.env.GITHUB_REPOSITORY
+  if (!repository)
+    throw new Error('GITHUB_REPOSITORY is not set in an environment variable.')
+  if (!/^.+\/.*$/.test(repository))
+    throw new Error('GITHUB_REPOSITORY is not match pattern owner/name.')
+  return { eventName, repository }
+}
