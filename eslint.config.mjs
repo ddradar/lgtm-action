9518e0607ca78a8ab23f087c24d1bbfb11f90f9b
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-01-26 09:04:54 +0900

    chore(lint): disable vitest eslint config & ignore experimental `node:test` feature

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import eslint from '@eslint/js'
-import vitest from '@vitest/eslint-plugin'
 import { defineConfig } from 'eslint/config'
 import prettier from 'eslint-config-prettier'
 import node from 'eslint-plugin-n'
@@ -14,6 +13,12 @@ export default defineConfig(
   {
     ...node.configs['flat/recommended-script'],
     settings: { n: { allowModules: ['@octokit/webhooks-types'] } },
+    rules: {
+      'n/no-unsupported-features/node-builtins': [
+        'error',
+        { ignores: ['test', 'test.mock', 'test.mock.module'] },
+      ],
+    },
   },
   // TypeScript
   {
@@ -27,32 +32,6 @@ export default defineConfig(
   { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' } },
   // Prettier
   prettier,
-  // Vitest
-  {
-    files: ['**/*.test.ts'],
-    plugins: vitest.configs.recommended.plugins,
-    rules: {
-      ...vitest.configs.recommended.rules,
-      'vitest/consistent-test-filename': 'error',
-      'vitest/consistent-test-it': ['error', { fn: 'test' }],
-      'vitest/no-alias-methods': 'error',
-      'vitest/no-conditional-in-test': 'error',
-      'vitest/no-disabled-tests': 'warn',
-      'vitest/no-duplicate-hooks': 'error',
-      'vitest/no-focused-tests': 'error',
-      'vitest/no-import-node-test': 'error',
-      'vitest/no-standalone-expect': 'error',
-      'vitest/no-test-return-statement': 'error',
-      'vitest/prefer-comparison-matcher': 'error',
-      'vitest/prefer-each': 'error',
-      'vitest/prefer-equality-matcher': 'error',
-      'vitest/prefer-hooks-in-order': 'error',
-      'vitest/prefer-hooks-on-top': 'error',
-      'vitest/prefer-mock-promise-shorthand': 'error',
-      'vitest/prefer-strict-equal': 'error',
-      'vitest/require-to-throw-message': 'error',
-    },
-  },
   // simple-import-sort
   {
     plugins: { 'simple-import-sort': simpleImportSort },
